9408bd94f84b4a722fcc27dce9a1a3747b458545
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-04-20 13:14:41 -0300

    Added project files

---
+++ IamRich/IamRichTests/IamRichTests.swift
@@ -0,0 +1,33 @@
+//
+//  IamRichTests.swift
+//  IamRichTests
+//
+//  Created by Lucas Werner Kuipers on 20/04/2021.
+//
+
+import XCTest
+@testable import IamRich
+
+class IamRichTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
